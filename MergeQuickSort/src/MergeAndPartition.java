a37828efa82acb07dbdc37008eae4a44121962c3
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-02-05 09:23:19 -0500

    ssss

--- MergeQuickSort/src/MergeAndPartition.java
+++ MergeQuickSort/src/MergeAndPartition.java
@@ -7,23 +7,34 @@ public class MergeAndPartition
 		int i = 0;
 		int j = 0;
 		int k = 0;
+		int counter=0;
+		int indexOfList2 = 0;
 		while (i < list1.length)
 		{
 			if (list1[i] < list2[j])
 			{
 				list3[k] = list1[i];
 				k++;
+				counter++;
 			}
 			if (list1[i] > list2[j])
 			{
 				list3[k] = list2[j];
 				k++;
 				j++;
+				i--;
+				counter++;
+				indexOfList2++;
 			}
 		
 		i++;
 		}
-	
+		
+		for (indexOfList2; indexOfList2 < List2.length; indexOfList2++)
+		{
+			list3[counter] = list2[indexOfList2];
+		}
+		
 	return list3;
 	}
 }
