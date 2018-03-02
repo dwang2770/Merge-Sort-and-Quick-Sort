7da9cb1931d428bd75a2555ede83d329fc58dda4
dwang2770 <32362393+dwang2770@users.noreply.github.com>
2018-03-02 16:33:31 -0500

    Update MergeSort.java

--- MergeQuickSort/src/MergeSort.java
+++ MergeQuickSort/src/MergeSort.java
@@ -5,11 +5,13 @@ public class MergeSort
 	public static String [] mergeSorts(String [] list)
 	{
 		
-		
+		//once the array is split to length one, we simply return the array
 		if (list.length==1) 
 		{
 			return list;
 		}
+		//we split the array in half as indicated by list2 and list 3
+		//we then continue to split them until length is one then we merge them back together
 		else 
 		{
 			String [] list2 = Arrays.copyOfRange(list, 0, list.length/2);
